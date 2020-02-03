f52bfbc9e048fa667bef289f419284145eb36b40
Mior Muhammad Zaki <crynobone@gmail.com>
2020-02-03 16:44:01 +0800

    Use laravel new Illuminate\Console\Concerns\HasParameters.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Commands/Command.php
+++ src/Commands/Command.php
@@ -3,6 +3,7 @@
 namespace Orchestra\Canvas\Core\Commands;
 
 use Illuminate\Console\Concerns\CallsCommands;
+use Illuminate\Console\Concerns\HasParameters;
 use Illuminate\Console\OutputStyle;
 use Orchestra\Canvas\Core\Presets\Preset;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,6 +12,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 abstract class Command extends \Symfony\Component\Console\Command\Command
 {
     use CallsCommands,
+        HasParameters,
         Concerns\InteractionsWithIO;
 
     /**
@@ -45,33 +47,6 @@ abstract class Command extends \Symfony\Component\Console\Command\Command
     }
 
     /**
-     * Specify the arguments and options on the command.
-     *
-     * @return void
-     */
-    protected function specifyParameters()
-    {
-        // We will loop through all of the arguments and options for the command and
-        // set them all on the base command instance. This specifies what can get
-        // passed into these commands as "parameters" to control the execution.
-        foreach ($this->getArguments() as $arguments) {
-            if ($arguments instanceof InputArgument) {
-                $this->getDefinition()->addArgument($arguments);
-            } else {
-                \call_user_func_array([$this, 'addArgument'], $arguments);
-            }
-        }
-
-        foreach ($this->getOptions() as $options) {
-            if ($options instanceof InputOption) {
-                $this->getDefinition()->addOption($options);
-            } else {
-                \call_user_func_array([$this, 'addOption'], $options);
-            }
-        }
-    }
-
-    /**
      * Resolve the console command instance for the given command.
      *
      * @param  \Symfony\Component\Console\Command\Command|string  $command
@@ -81,25 +56,5 @@ abstract class Command extends \Symfony\Component\Console\Command\Command
     protected function resolveCommand($command)
     {
         return $this->getApplication()->find($command);
-    }
-
-    /**
-     * Get the console command arguments.
-     *
-     * @return array
-     */
-    protected function getArguments()
-    {
-        return [];
-    }
-
-    /**
-     * Get the console command options.
-     *
-     * @return array
-     */
-    protected function getOptions()
-    {
-        return [];
     }
 }
