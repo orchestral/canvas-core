8c5ea3972c73bdcc5accbb56b950a3947450a96a
Mior Muhammad Zaki <crynobone@gmail.com>
2020-02-07 08:09:24 +0800

    Use Illuminate\Console\Concerns\InteractsWithIO.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Commands/Command.php
+++ src/Commands/Command.php
@@ -4,6 +4,7 @@ namespace Orchestra\Canvas\Core\Commands;
 
 use Illuminate\Console\Concerns\CallsCommands;
 use Illuminate\Console\Concerns\HasParameters;
+use Illuminate\Console\Concerns\InteractsWithIO;
 use Illuminate\Console\OutputStyle;
 use Orchestra\Canvas\Core\Presets\Preset;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,7 +14,7 @@ abstract class Command extends \Symfony\Component\Console\Command\Command
 {
     use CallsCommands,
         HasParameters,
-        Concerns\InteractionsWithIO;
+        InteractsWithIO;
 
     /**
      * Canvas preset.
