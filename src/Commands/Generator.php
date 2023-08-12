0754f0266630eefe0df82f22ad857c3bda422f17
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-13 06:32:50 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Commands/Generator.php
+++ src/Commands/Generator.php
@@ -11,8 +11,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * @property string  $name
- * @property string  $description
+ * @property string|null  $name
+ * @property string|null  $description
  */
 abstract class Generator extends Command implements GeneratesCodeListener
 {
