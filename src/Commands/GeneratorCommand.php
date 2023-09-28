2ae4333c779d1910e1b65325028908c2b56e0335
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-28 09:02:52 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Commands/GeneratorCommand.php
+++ src/Commands/GeneratorCommand.php
@@ -4,41 +4,14 @@ namespace Orchestra\Canvas\Core\Commands;
 
 use Illuminate\Filesystem\Filesystem;
 use Orchestra\Canvas\Core\Concerns;
-use Orchestra\Canvas\Core\Contracts\GeneratesCode;
 
 /**
  * @property string|null  $name
  * @property string|null  $description
  */
-abstract class GeneratorCommand extends \Illuminate\Console\GeneratorCommand implements GeneratesCode
+abstract class GeneratorCommand extends \Illuminate\Console\GeneratorCommand
 {
-    use Concerns\CodeGenerator;
-    use Concerns\TestGenerator;
     use Concerns\UsesGeneratorOverrides;
-
-    /**
-     * Create a new controller creator command instance.
-     *
-     * @return void
-     */
-    public function __construct(Filesystem $files)
-    {
-        parent::__construct($files);
-
-        $this->addGeneratorPresetOptions();
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return bool|null
-     *
-     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
-     */
-    public function handle()
-    {
-        return $this->generateCode() ? self::SUCCESS : self::FAILURE;
-    }
 
     /**
      * Get the destination class path.
