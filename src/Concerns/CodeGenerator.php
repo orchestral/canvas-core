91ecbe57dd77a1e65181aee9561ea834fa698a87
Mior Muhammad Zaki <crynobone@gmail.com>
2025-02-15 19:28:59 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/CodeGenerator.php
+++ src/Concerns/CodeGenerator.php
@@ -24,9 +24,7 @@ trait CodeGenerator
         // language and that the class name will actually be valid. If it is not valid we
         // can error now and prevent from polluting the filesystem using invalid files.
         if ($this->isReservedName($name)) {
-            $this->components->error(
-                \sprintf('The name "%s" is reserved by PHP.', $name)
-            );
+            $this->components->error(\sprintf('The name "%s" is reserved by PHP.', $name));
 
             return false;
         }
